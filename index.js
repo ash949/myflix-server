c10a2ef59a9dcc042331b2a30ced17012f7a0900
Hamza Ashour <hamza.ashour.work@gmail.com>
2021-03-25 11:43:03 +0200

    project basic setup

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('common'));
+
+let movies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+app.use(express.static('public'));
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movies club!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
